324b81f4aba8098f9582f2ffea320b52eaab39a5
Brian Wylie <briford.wylie@gmail.com>
2020-09-19 16:27:03 -0600

    adding led control class

---
+++ robot_kit/led.py
@@ -0,0 +1,54 @@
+"""A lightweight wrapper around the NeoPixel LED Strip"""
+import board
+import neopixel
+
+
+class NeoPixelStrip():
+    """A lightweight wrapper around the NeoPixel LED Strip
+       Usage:
+            led_strip = NeoPixelStrip()
+            led_strip.on(255, 0, 0)  # R, G, B
+            time.sleep(1)
+            led_strip.off()
+    """
+    def __init__(self, led_pin=board.D18, num_leds=8, brightness=1.0):
+        """NeoPixelStrip Initialization"""
+        self.led_pin = led_pin
+        self.num_leds = num_leds
+        self.brightness = brightness
+        self.strip = neopixel.NeoPixel(self.led_pin, self.num_leds, brightness=self.brightness)
+
+    def on(self, red, green, blue):
+        """Turn all the LEDs to one color
+        Args:
+               red: red value (0-255)
+               green: green value (0-255)
+               blue: blue value (0-255)
+        """
+        self.strip.fill((red, green, blue))
+
+    def off(self):
+        """Turn all the LEDs off"""
+        self.strip.fill((0, 0, 0))
+
+    def cleanup(self):
+        """Method that's called when class is destroyed"""
+        print('Cleanup...')
+        self.off()
+        self.strip.deinit()
+
+
+def test():
+    """Test for the NeoPixelStrip class"""
+    import time
+    led_strip = NeoPixelStrip()
+    led_strip.on(255, 0, 0)  # R, G, B
+    time.sleep(1)
+    led_strip.off()
+    led_strip.cleanup()
+
+
+if __name__ == '__main__':
+
+    # Run the test
+    test()
