791e82fe060e1bd5f690f1d6a4a461d4e1b0aa20
Brian Wylie <briford.wylie@gmail.com>
2020-09-22 10:42:29 -0600

    add an example script and update readme

--- examples/robot_start.py
+++ examples/robot_start.py
@@ -1,20 +1,8 @@
-# robot_kit
-Having fun with the Freenove 4WD Smart Car Kit
-
-### Install
-SSH to your Raspberry Pi and install the Python module for Python 3.
-
-```
-$ sudo pip3 install robot_kit
-```
-
-### Examples
-See the Examples directory for all the code, here are some short examples of how to use the module.
-
-```
+"""An example script that uses the functionality of robot_kit"""
 from robot_kit.leds import NeoPixelStrip
 from robot_kit.wheels import Wheels
 import time
+
 
 if __name__ == '__main__':
     """Run some of the basic commands for robot_kit"""
@@ -34,4 +22,3 @@ if __name__ == '__main__':
     wheels.all(0.25)
     time.sleep(1.0)
     wheels.stop()
-```
